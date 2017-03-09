cf5e85943511a94d576ceae27e48455a7e4ad4ad
lipinghui <lawrleegle@gmail.com>
2017-03-09 10:48:47 +0800

    add readme and disable SELinux

--- install_kubeadm_ubuntu16.sh
+++ install_kubeadm_ubuntu16.sh
@@ -6,6 +6,9 @@ cat <<EOF > /etc/apt/sources.list.d/kubernetes.list
 deb http://apt.kubernetes.io/ kubernetes-xenial main
 EOF
 apt-get update
+# Disable SELinux
+apt install selinux-utils
+setenforce 0
 # Install docker if you don't have it already.
 apt-get install -y docker.io
 apt-get install -y kubelet kubeadm kubectl kubernetes-cni

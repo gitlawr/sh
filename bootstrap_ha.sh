474443d680d5439b1610ae1019d655ef38e2734a
gitlawr <lawrleegle@gmail.com>
2019-04-29 08:29:34 +0800

    Update bootstrap_ha.sh

--- bootstrap_ha.sh
+++ bootstrap_ha.sh
@@ -1,17 +1,18 @@
 
 #install docker to existing vm
-if [ "$DEV_HOST" = "" ]; then
-echo "Please provide node IP for DEV_HOST:"
+echo "Please provide a node IP to bootstrap HA Rancher:"
 read DEV_HOST
-fi
-awssh ubuntu@$DEV_HOST <<'ENDSSH'
+echo "Please provide FQDN for Rancher:"
+read RANCHER_HOSTNAME
+
+ssh -o "StrictHostKeyChecking no" ubuntu@$DEV_HOST <<'ENDSSH'
 curl https://releases.rancher.com/install-docker/18.09.sh | sh
 sudo usermod -aG docker ubuntu
 ENDSSH
 
 # install kubernetes cluster
 rke up
-cpkb
+cp ./kube_config_cluster.yml ~/.kube/config
 
 # helm init
 kubectl -n kube-system create serviceaccount tiller
@@ -21,6 +22,9 @@ kubectl create clusterrolebinding tiller \
 hlm init --service-account tiller
 kubectl -n kube-system  rollout status deploy/tiller-deploy
 
+# update repo
+hlm repo update
+
 # install cert-manager
 hlm install stable/cert-manager \
   --name cert-manager \
@@ -29,10 +33,6 @@ hlm install stable/cert-manager \
 kubectl -n kube-system rollout status deploy/cert-manager
 
 # install rancher
-if [ "$RANCHER_HOSTNAME" = "" ]; then
-echo "Please provide FQDN for Rancher:"
-read RANCHER_HOSTNAME
-fi
 echo "Using \"$RANCHER_HOSTNAME\" as the FQDN for Rancher."
 hlm install rancher-latest/rancher \
   --name rancher \
